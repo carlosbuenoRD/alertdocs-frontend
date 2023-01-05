2239d08eda5f38b9d3ef1964aad3f610f4a08a02
Carlos Bueno <developer.carlosbueno@gmail.com>
2023-01-05 19:23:28 -0400

    layout structure done! and defaults css classes changed

--- src/components/layout/AppTopbar.tsx
+++ src/components/layout/AppTopbar.tsx
@@ -1,29 +1,29 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import AppMenu from "./AppMenu";
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import AppMenu from './AppMenu'
 const AppTopbar = (props: any) => {
   const onTopbarSubItemClick = (event: any) => {
-    event.preventDefault();
-  };
+    event.preventDefault()
+  }
 
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   return (
     <>
-      <div className="layout-topbar">
-        <div className="layout-topbar-left">
+      <div className='layout-topbar'>
+        <div className='layout-topbar-left'>
           <button
-            className="topbar-menu-button p-link"
+            className='topbar-menu-button p-link'
             onClick={props.onMenuButtonClick}
           >
-            <i className="pi pi-bars"></i>
+            <i className='pi pi-bars'></i>
           </button>
 
-          <button className="logo p-link" onClick={() => navigate("/")}>
-            <img src={`assets/images/Logo-cupula.png`} alt="logo" />
+          <button className='logo p-link' onClick={() => navigate('/')}>
+            <img src={`assets/images/Logo-cupula.png`} alt='logo' />
           </button>
 
-          <h4 className="m-0 ml-2 uppercase">Alertdoc</h4>
+          <h4 className='m-0 ml-2 uppercase text-white'>Alertdoc</h4>
         </div>
 
         <AppMenu
@@ -42,7 +42,7 @@ const AppTopbar = (props: any) => {
         />
       </div>
     </>
-  );
-};
+  )
+}
 
-export default AppTopbar;
+export default AppTopbar
